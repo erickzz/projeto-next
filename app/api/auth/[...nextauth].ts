ea96b1ca4e6952a31e647892a8cc6fd28b6c1e1d
erickzz <erick.reis2@gmail.com>
2024-02-29 12:53:35 -0300

    Included Google Provider

--- app/api/auth/[...nextauth].ts
+++ app/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google";
+
 
 export default NextAuth({
   providers: [
@@ -7,5 +9,9 @@ export default NextAuth({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    })
   ],
 })
