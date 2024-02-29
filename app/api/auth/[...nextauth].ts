c865f40bb8a66e13f9d399b38cfd4e99b6b9dbcb
erickzz <erick.reis2@gmail.com>
2024-02-29 14:26:11 -0300

    Define Google Provider authorization

--- app/api/auth/[...nextauth].ts
+++ app/api/auth/[...nextauth].ts
@@ -1,14 +1,21 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from 'next-auth/providers/credentials'
+import EmailProvider from "next-auth/providers/email";
 
 
 
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
     })
   ],
   session:{
